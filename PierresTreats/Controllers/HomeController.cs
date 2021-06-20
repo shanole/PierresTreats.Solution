91bdc7080c7c33ac1196b1bae371d7898a74ad53
shanole <73411129+shanole@users.noreply.github.com>
2021-06-20 00:31:49 -0700

    add list of flavors and treats to home page

--- PierresTreats/Controllers/HomeController.cs
+++ PierresTreats/Controllers/HomeController.cs
@@ -1,12 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
+using PierresTreats.Models;
+using System.Linq;
 
 namespace PierresTreats.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly PierresTreatsContext _db;
+    public HomeController(PierresTreatsContext db)
+    {
+      _db = db;
+    }
     [HttpGet("/")]
     public ActionResult Index()
     {
+      ViewBag.TreatsList = _db.Treats.OrderBy(m => m.Name).ToList();
+      ViewBag.FlavorsList = _db.Flavors.OrderBy(m => m.Description).ToList();
       return View();
     }
   }
