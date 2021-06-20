7f300622a9088d03f7999e8411985fb089baff38
shanole <73411129+shanole@users.noreply.github.com>
2021-06-20 01:02:16 -0700

    remove UserName field

--- PierresTreats/ViewModels/RegisterViewModel.cs
+++ PierresTreats/ViewModels/RegisterViewModel.cs
@@ -4,10 +4,7 @@ namespace PierresTreats.ViewModels
 {
   public class RegisterViewModel
   {
-    [Required]
-    [Display(Name = "User Name")]
-    public string UserName { get; set; }
-    
+
     [Required]
     [EmailAddress]
     [Display(Name = "Email")]
